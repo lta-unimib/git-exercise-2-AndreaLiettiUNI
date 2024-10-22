5dca02d85251e312c8fcb2ab17baaef4a53ca581
a.lietti4@campus.unimib.it <a.lietti4@campus.unimib.it>
2024-10-22 15:26:42 +0200

    sbagliata la commit precedente

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -3,6 +3,6 @@ package gitExercise2.main;
 public class HelloGit {
 	public static void main (String [] args) {
 		System.out.println("Hello git!!!");
-		System.out.println("Hello2!!! seconda modifica prr simulare la modifica simultanea di una riga di codice");
+		System.out.println("Hello2!!!");
 	}
 }
